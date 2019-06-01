f4eabd7754cb0399cdf4837ee0afe850d8e0760a
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-06-01 14:04:07 +0700

    nnti kulanjutkan gaes

--- application/views/userview/halamanprofil.php
+++ application/views/userview/halamanprofil.php
@@ -1,26 +1,33 @@
-  <div class="row">
-      <div class="col-md-12"  >
-      <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2em">
-        <h4 style="color: #2F669F;">Selamat Datang di</h4>
-        <h3 style="color: #2F669F">Sistem Sirkulasi Pengawas Ujian</h3>
-      </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 50px;">
-          <h3  style="color: #2F669F; padding-top:10px; padding-left: 10px;">Tentang</h3>
-          <p style="padding: 10px 10px 10px 10px;">Sistem Sirkulasi Pengawas Ujian adalah sebuah sistem aplikasi berbasis web ini untuk menetapkan jadwal penugasan pengawas ujian UTBK , jadwal pelatihan , dan koordinatornya.</p>
-          <p style="padding-top: -15px; padding-left: 10px;">
-            Tidak hanya itu , aplikasi ini dilengkapi berbagai fitur yang memudahkan para pengawas dan koordinator dalam melihat jadwal mereka , baik itu jadwal mengawas ujian dan jadwal pelatihan sesuai dengan waktu yang telah ditetapkan.
-          </p>
-        </div>
-      </div>  
+<div class="row">
+  <div class="col-md-12"> 
+    <div class="container" style="border-width: 0px; ">
       <div class="row">
-      <div class="container-fluid fixed-bottom">
-        <div style="background-color: #ffff; color: grey; padding: 15px 25px 10px 20px; margin-top: 69px; margin-left: -15px;margin-right: -15px;height: 40px">
-          <h6>Copyright &reg; Kelompok ? - Manajemen Sistem Basis Data 2019 - Teknologi Informasi - Universitas Sumatera Utara - 2019/2020</h6>
+        <div class="col-md-6">
+
+          <div class="container" style="background-color:#0e5fa4; border-width: 0px; height:250px;  width: 250px; margin-top: 100px; margin-left: 200px;">
+            <img src="<?php echo base_url()?>img/icon/pegawai_2.png" style="margin-top:0px;margin-left:60px;max-width: 100px; max-height: 100px;">
+            <center style="font-family: calibri;font-size: 34px;margin-top: 10px;">namauser</center>
+             <center style="font-family: calibri;font-size: 16px; margin-top:30px;">EDIT PROFIL</center>
+          </div>
         </div>
-         
-      </div>
-    </div>
+        <div class="col-md-6">
+            <div class="container" style="background-color:#127cd7; border-width: 0px; height:250px;  width: 500px; margin-top: 100px; margin-right:300px;">
+              <table id="myTable">
+            <tr>
+              <td>NIP</td>
+            </tr>
+            <tr>
+              <td>NAMA</td>
+            </tr>
+            <tr>
+              <td>NAMA UNIT</td>
+            </tr>
+            <tr>
+              <td>STATUS</td>
+            </tr>
+            <tr>
+              <td>JABATAN</td>
+            </tr>
+          </div>
+          </div>
+        </div>
