5a111db3c8a09df0663be3c10fa7dcc6efaadce4
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-06-01 12:21:32 +0700

    login

--- application/views/userview/loginadmin.php
+++ application/views/userview/loginadmin.php
@@ -1,26 +1,70 @@
-  <div class="row">
-      <div class="col-md-12"  >
-      <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2em">
-        <h4 style="color: #2F669F;">Selamat Datang di</h4>
-        <h3 style="color: #2F669F">Sistem Sirkulasi Pengawas Ujian</h3>
-      </div>
+<style type="text/css">h5{
+  color: white;
+  }h4{
+  color: white;
+  }input {
+  border: 0;
+  outline: 0;
+  background: transparent;
+  border-bottom: 1px solid black;
+  }</style>
+<div class="col-md-12">
+  <center>
+    <div class="row" style="margin-top: 80px;height: 500px;">
+      <div class="col-md-5 offset-md-3" style="padding-left: 100px;">
+        <form method="post" action="logpro">
+          <div class="container" style="border-color: white;border-style: solid;background-color: #127cd7;border-radius: 10px;
+          ">
+          <div class="row">
+            <div class="container-fluid">
+          <img src="<?php echo base_url()?>img/icon/pegawai_2.png" style="margin-top: -50px;max-width: 140px; max-height: 140px;">
+            </div>
+          </div>
+          <div class="row" style="margin-bottom: 10px;">
+            <div class="container-fluid">
+                <h4 style="color: white;">Masuk</h4>
+
+          <div class="row" style="margin-top: 20px;">
+            <div class="container-fluid">                     
+                <h6 style="color:white; text-align: left;">&nbsp;&nbsp;Nama Pengguna</h6>
+            </div>
+          </div>
+          <div class="row">
+            <div class="container-fluid">
+                <input style="width: 92%" type="text" name="username" placeholder="Nama Pengguna" checked>
+            </div>
+          </div>
+          <div class="row" style="margin-top: 20px;">
+            <div class="container-fluid">
+                <h6 style="color:white; text-align: left;">&nbsp;&nbsp;Kata Sandi</h6>
+            </div>
+          </div>
+          <div class="row">
+            <div class="container-fluid">
+                <input style="width: 92%" type="password" name="password" placeholder="Kata Sandi" id="myInput">
+            </div>
+          </div>
+          <div class="row" style="margin-top: 5px; padding-left: 10px;" align="left" >
+            <div class="container-fluid" style="color: white;">
+                  <td>
+                    <span style="margin-left: 5px;">Lupa Kata Sandi?</span>
+                  </td>
+                </tr>
+              </table>
+              
+              
+            </div>
+          </div>
+          <div class="row" style="margin-top: 30px; margin-bottom: 20px;">
+            <div class="container-fluid">
+                <button class="btn btn-primary shadow" type="submit" style="width: 100px; height: 40px;">Masuk</button>
+            </div>
+          </div>    
+          </div>
+          </form>   
+          </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 50px;">
-          <h3  style="color: #2F669F; padding-top:10px; padding-left: 10px;">Tentang</h3>
-          <p style="padding: 10px 10px 10px 10px;">Sistem Sirkulasi Pengawas Ujian adalah sebuah sistem aplikasi berbasis web ini untuk menetapkan jadwal penugasan pengawas ujian UTBK , jadwal pelatihan , dan koordinatornya.</p>
-          <p style="padding-top: -15px; padding-left: 10px;">
-            Tidak hanya itu , aplikasi ini dilengkapi berbagai fitur yang memudahkan para pengawas dan koordinator dalam melihat jadwal mereka , baik itu jadwal mengawas ujian dan jadwal pelatihan sesuai dengan waktu yang telah ditetapkan.
-          </p>
-        </div>
-      </div>  
-      <div class="row">
-      <div class="container-fluid fixed-bottom">
-        <div style="background-color: #ffff; color: grey; padding: 15px 25px 10px 20px; margin-top: 69px; margin-left: -15px;margin-right: -15px;height: 40px">
-          <h6>Copyright &reg; Kelompok ? - Manajemen Sistem Basis Data 2019 - Teknologi Informasi - Universitas Sumatera Utara - 2019/2020</h6>
-        </div>
-         
-      </div>
-    </div>
+  </center>
+                
+</div>
