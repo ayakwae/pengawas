78f72094f84ad6401438f9dd89430098811e877d
nadiasitinamira <nadiasitinamira@gmail.com>
2019-06-02 20:35:58 +0700

    menu navbar atas

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -2,22 +2,40 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbar"  style="margin-top: -1px; border-style: solid;border-color: black; border-width: 0px; margin-bottom: -10px; background-color: #DCDCDC;">
+  <div class="collapse navbar-collapse" id="navbar"  style="margin-top: -1px; border-style: solid;border-color: black; border-width: 0px; margin-bottom: -15px; background-color: #DCDCDC;">
     <ul class="navbar-nav mx-auto">
       <li class="nav-item">
-        <a class="nav-link text-expanded" href="<?php echo base_url()?>" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-right: 25px;">Beranda<span class="sr-only">(current)</span></a>
+        <p class="navbar-nav" style="padding-top: 5px; color: gray;">|</p>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-expanded" href="<?php echo base_url()?>daftarpengawas" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-right: 25px; margin-left: 25px;">Daftar Pengawas</a>
+        <a class="nav-link text-expanded" href="<?php echo base_url()?>" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-right: 25px; margin-left: 25px;">Beranda<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-expanded" href="<?php echo base_url()?>daftarkoordinator" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-right: 25px; margin-left: 25px;">Daftar Koordinator</a>
+        <p class="navbar-nav" style="padding-top: 5px; color: gray; margin-right: 20px;">|</p>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-expanded" href="<?php echo base_url()?>halamanreport" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-right: 25px; margin-left: 25px;">Report</a>
+        <a class="nav-link text-expanded" href="<?php echo base_url()?>daftarpengawas" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-right: 35px; margin-left: 20px;">Daftar Pengawas</a>
+      </li>
+      <li class="nav-item">
+        <p class="navbar-nav" style="padding-top: 5px; color: gray; margin-right: 20px;">|</p>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-expanded" href="<?php echo base_url()?>daftarkoordinator" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-right: 40px; margin-left: 20px;">Daftar Koordinator</a>
+      </li>
+      <li class="nav-item">
+        <p class="navbar-nav" style="padding-top: 5px; color: gray; margin-right: 20px;">|</p>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-expanded" href="<?php echo base_url()?>halamanreport" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-right: 50px; margin-left: 25px;">Report</a>
+      </li>
+      <li class="nav-item">
+        <p class="navbar-nav" style="padding-top: 5px; color: gray; margin-right: 0px;">|</p>
       </li>
       <li class="nav-item text-expanded">
-        <a class="nav-link" href="<?php echo base_url()?>loginadmin" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-left: 25px;">Masuk</a>
+        <a class="nav-link" href="<?php echo base_url()?>loginadmin" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-left: 25px; margin-right: 25px;">Masuk</a>
+      </li>
+      <li class="nav-item">
+        <p class="navbar-nav" style="padding-top: 5px; color: gray;">|</p>
       </li>
     </ul>
   </div>
