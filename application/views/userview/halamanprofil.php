204da1b1c2a344207085545d2d971b7359d56d61
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-06-03 11:23:53 +0700

    halaman profil

--- application/views/userview/halamanprofil.php
+++ application/views/userview/halamanprofil.php
@@ -1,3 +1,49 @@
+<style>
+#box1{
+        margin-bottom: 10px;
+        padding-bottom: 25px;
+        width:450px;
+        height:20px;
+        background:#127cd7;
+        border:solid 0px black;
+      }
+#box2{
+        margin-bottom: 10px;
+        padding-bottom: 25px;
+        width:450px;
+        height:20px;
+        background:#127cd7;
+      }
+#box3{
+        margin-bottom: 10px;
+        padding-bottom: 25px;
+        width:450px;
+        height:20px;
+        background:#127cd7;
+      }
+#box4{
+        margin-bottom: 10px;
+        padding-bottom: 25px;
+        width:450px;
+        height:20px;
+        background:#127cd7;
+        
+      }
+#box5{
+        margin-bottom: 10px;
+        padding-bottom: 25px;
+        width:450px;
+        height:20px;
+        background:#127cd7;
+      }
+#box6{
+        margin-bottom: 10px;
+        padding-bottom: 25px;
+        width:100px;
+        height:20px;
+        background:#0066cc;
+      }
+</style>
 <div class="row">
   <div class="col-md-12"> 
     <div class="container" style="border-width: 0px; ">
@@ -7,27 +53,28 @@
           <div class="container" style="background-color:#0e5fa4; border-width: 0px; height:250px;  width: 250px; margin-top: 100px; margin-left: 200px;">
             <img src="<?php echo base_url()?>img/icon/pegawai_2.png" style="margin-top:0px;margin-left:60px;max-width: 100px; max-height: 100px;">
             <center style="font-family: calibri;font-size: 34px;margin-top: 10px;">namauser</center>
-             <center style="font-family: calibri;font-size: 16px; margin-top:30px;">EDIT PROFIL</center>
+             <center><div id="box6" center style="font-family: calibri;font-size: 16px; margin-top:30px;">EDIT PROFIL</div></center>
           </div>
         </div>
         <div class="col-md-6">
-            <div class="container" style="background-color:#127cd7; border-width: 0px; height:250px;  width: 500px; margin-top: 100px; margin-right:300px;">
+            <div class="container" style="background-color:#0066cc; border-width: 0px; height:250px;  width: 500px; margin-top: 100px; margin-right:300px;padding-top: 20px;">
               <table id="myTable">
             <tr>
-              <td>NIP</td>
+             <b><div id="box1">NIP</div></b>
             </tr>
             <tr>
-              <td>NAMA</td>
+              <b><div id="box2">NAMA</div></b>
             </tr>
             <tr>
-              <td>NAMA UNIT</td>
+              <b><div id="box3">NAMA UNIT</div></b>
             </tr>
             <tr>
-              <td>STATUS</td>
+              <b><div id="box4">STATUS</div></b>
             </tr>
             <tr>
-              <td>JABATAN</td>
+              <b><div id="box5">JABATAN</div></b>
             </tr>
+            <img src="<?php echo base_url()?>img/icon/keluar.png" style="margin-top:20px;margin-left:450px;max-width: 30px; max-height: 60px;">
           </div>
           </div>
         </div>
