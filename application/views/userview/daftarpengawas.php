cc1a6a7feb1ea837c087c5e29a5e16e10eaa984b
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-06-03 14:07:54 +0700

    aaaaaaaaaa

--- application/views/userview/daftarpengawas.php
+++ application/views/userview/daftarpengawas.php
@@ -1,26 +1,63 @@
-  <div class="row">
-      <div class="col-md-12"  >
-      <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2em">
-        <h4 style="color: #2F669F;">Selamat Datang di</h4>
-        <h3 style="color: #2F669F">Sistem Sirkulasi Pengawas Ujian</h3>
+<style> 
+
+form {
+    overflow: hidden;
+}
+ 
+label {
+    float: left;
+    width: 120px;
+    padding-right: 24px;
+
+}
+ 
+input {
+    float: left;
+    width: calc(100% - 200px);
+    margin-bottom: 10px;
+    border:solid 1px;
+}
+ 
+button {
+    float: right;
+    width: calc(100% - 200px);
+}
+
+</style>
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2.5em; border:solid 1px;margin-bottom: 10px;">
+      <div class="row" style="margin-left: 10px; margin-right: 100px;">
+        <table id="myTable">
+            <form>
+    <label for="hari&tanggal" class="hari-tanggal">Hari/Tanggal</label>
+    <input id="hari&tanggal" type="text">
+ 
+    <label for="jam" class="Jam">Jam</label>
+    <input id="jam" type="text">
+
+    <label for="tgl" class="tanggal">Tanggal</label>
+    <input id="tgl" type="text">
+ 
+</form>
+    <a href="<?php base_url() ?>search">
+      <div class="container" style="background-color:#8dcff4; border-width: 0px; border-radius: 10px; height:40px;  width: 150px; margin-left: 900px;">
+ <img src="<?php echo base_url()?>img/icon/search.png" style="margin-top:5px;margin-left:1px;max-width: 30px; max-height: 100px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cari
       </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 50px;">
-          <h3  style="color: #2F669F; padding-top:10px; padding-left: 10px;">Tentang</h3>
-          <p style="padding: 10px 10px 10px 10px;">Sistem Sirkulasi Pengawas Ujian adalah sebuah sistem aplikasi berbasis web ini untuk menetapkan jadwal penugasan pengawas ujian UTBK , jadwal pelatihan , dan koordinatornya.</p>
-          <p style="padding-top: -15px; padding-left: 10px;">
-            Tidak hanya itu , aplikasi ini dilengkapi berbagai fitur yang memudahkan para pengawas dan koordinator dalam melihat jadwal mereka , baik itu jadwal mengawas ujian dan jadwal pelatihan sesuai dengan waktu yang telah ditetapkan.
-          </p>
-        </div>
+    </a>
+      </table>
       </div>  
-      <div class="row">
-      <div class="container-fluid fixed-bottom">
-        <div style="background-color: #ffff; color: grey; padding: 15px 25px 10px 20px; margin-top: 69px; margin-left: -15px;margin-right: -15px;height: 40px">
-          <h6>Copyright &reg; Kelompok ? - Manajemen Sistem Basis Data 2019 - Teknologi Informasi - Universitas Sumatera Utara - 2019/2020</h6>
-        </div>
-         
-      </div>
     </div>
+  </div>
+</div>
+<a href="<?php base_url() ?>tambah">
+<div class="container" style="background-color:#b4b4b4; border-width: 0px; border-radius: 10px; height:40px;  width: 150px; margin-left: 0px;">
+<img src="<?php echo base_url()?>img/icon/tambah.png" style="margin-top:5px;margin-bottom:10px;margin-left:5px;max-width: 30px; max-height: 100px;">&nbsp;&nbsp;&nbsp;Tambah
+</div>
+</a>
+<table>
+ 
+</table>
+
+        
