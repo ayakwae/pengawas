49667b4937017d5e1427480296a1adf957e6d252
nadiasitinamira <nadiasitinamira@gmail.com>
2019-06-04 21:23:44 +0700

    profil

--- application/views/userview/halamanprofil.php
+++ application/views/userview/halamanprofil.php
@@ -1,84 +1,97 @@
-<style>
-#box1{
-        margin-bottom: 10px;
-        padding-bottom: 25px;
-        width:450px;
-        height:20px;
-        background:#127cd7;
-        border:solid 0px black;
-      }
-#box2{
-        margin-bottom: 10px;
-        padding-bottom: 25px;
-        width:450px;
-        height:20px;
-        background:#127cd7;
-      }
-#box3{
-        margin-bottom: 10px;
-        padding-bottom: 25px;
-        width:450px;
-        height:20px;
-        background:#127cd7;
-      }
-#box4{
-        margin-bottom: 10px;
-        padding-bottom: 25px;
-        width:450px;
-        height:20px;
-        background:#127cd7;
-        
-      }
-#box5{
-        margin-bottom: 10px;
-        padding-bottom: 25px;
-        width:450px;
-        height:20px;
-        background:#127cd7;
-      }
-#box6{
-        margin-bottom: 10px;
-        padding-bottom: 25px;
-        width:100px;
-        height:20px;
-        background:#0066cc;
-      }
+<style type="text/css">
+  .circle {
+      width: 180px;
+      height: 186px;
+      background: white;
+      border-radius: 50%
+    }
 </style>
-<div class="row">
-  <div class="col-md-12"> 
-    <div class="container" style="border-width: 0px; ">
-      <div class="row">
-        <div class="col-md-6">
 
-          <div class="container" style="background-color:#0e5fa4; border-width: 0px; height:250px;  width: 250px; margin-top: 100px; margin-left: 200px;">
-            <img src="<?php echo base_url()?>img/icon/pegawai_2.png" style="margin-top:0px;margin-left:60px;max-width: 100px; max-height: 100px;">
-            <center style="font-family: calibri;font-size: 34px;margin-top: 10px;">namauser</center>
-             <a href="<?php base_url() ?>editprofil">
-             <center><div id="box6" center style="font-family: calibri;font-size: 16px; margin-top:30px;">EDIT PROFIL</div></center>
-           </a>
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2.5em">
+      <div class="row" style="margin-left: 5px;">
+        <span style="background-color: #2F669F; height: 25px; width: 15px; margin-top: 7px; margin-left: 5px;"></span>
+        <span style="background-color: #2F669F; height: 25px; width: 5px; margin-top: 7px; margin-left: 5px;"></span> &nbsp;&nbsp;
+        <h3 style="color: #2F669F;"><b>Profil</b></h3>
+      </div>  
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;border-width: 1px;padding-bottom: 50px;">
+      <div class="row" style="margin-top: 25px; margin-left: 60px;">
+        <div class="col-md-6">
+        <div class="container" style="background-color:#1E90FF; border-width: 0px; height:470px;  width: 300px; margin-top: 40px; margin-left: 0px; padding-top: 1px;">
+          <div class="circle" style="margin: 30px 20px 20px 47px;">
+            <img src="<?php echo base_url()?>img/icon/pegawai_2.png" style="width: 190px; height: 190px; margin-top: -2px; margin-left: -4px;">
           </div>
+          <center style="font-family: calibri;font-size: 34px;margin-top: 10px; margin-bottom: 50px;"><b>-namauser-</b></center>
+          <center>
+            <div class="container" style="margin-bottom: 20px;">
+              <a href="<?php base_url() ?>editprofil">
+                <button type="button" class="btn btn-primary">Edit Profil</button>
+              </a>
+            </div>
+            <div>
+              <a href="<?php base_url() ?>ubahsandi">
+                <button type="button" class="btn btn-danger">Ubah Kata Sandi</button>
+              </a>
+            </div>
+          </center>
+        </div>
         </div>
         <div class="col-md-6">
-            <div class="container" style="background-color:#0066cc; border-width: 0px; height:250px;  width: 500px; margin-top: 100px; margin-right:300px;padding-top: 20px;">
-              <table id="myTable">
-            <tr>
-             <b><div id="box1">NIP</div></b>
-            </tr>
-            <tr>
-              <b><div id="box2">NAMA</div></b>
-            </tr>
-            <tr>
-              <b><div id="box3">NAMA UNIT</div></b>
-            </tr>
-            <tr>
-              <b><div id="box4">STATUS</div></b>
-            </tr>
-            <tr>
-              <b><div id="box5">JABATAN</div></b>
-            </tr>
-            <a href="<?php base_url() ?>keluar">
-            <img src="<?php echo base_url()?>img/icon/keluar.png" style="margin-top:20px;margin-left:450px;max-width: 30px; max-height: 60px;">
-          </a>
-          </div>
+            <div class="container" style="background-color:#1E90FF; border-width: 0px; height:470px;  width: 600px; margin-top: 40px; padding-top: 1px; margin-left: -180px;">
+              <div class="container" style="background-color:#F8F8F8; border-width: 0px; font-weight: 500; margin-bottom: 20px; margin-top: 30px;">
+                <table width="300">
+                  <tr>
+                    <td style="width: 170px;"><b>NIP</b></td>
+                    <td style="padding-left: 10px;"><?php ?></td>
+                  </tr>
+                </table>
+              </div>
+              <div class="container" style="background-color:#F8F8F8; border-width: 0px; font-weight: 500; margin-bottom: 20px;">
+                <table width="300">
+                  <tr>
+                    <td style="width: 170px;"><b>Nama Petugas</b></td>
+                    <td style="padding-left: 10px;"><?php ?></td>
+                  </tr>
+                </table>
+              </div>
+              <div class="container" style="background-color:#F8F8F8; border-width: 0px; font-weight: 500; margin-bottom: 20px;">
+                <table width="300">
+                  <tr>
+                    <td style="width: 170px;"><b>Nama Unit</b></td>
+                    <td style="padding-left: 10px;"><?php ?></td>
+                  </tr>
+                </table>
+              </div>
+              <div class="container" style="background-color:#F8F8F8; border-width: 0px; font-weight: 500; margin-bottom: 20px;">
+                <table width="300">
+                  <tr>
+                    <td style="width: 170px;"><b>Status</b></td>
+                    <td style="padding-left: 10px;"><?php ?></td>
+                  </tr>
+                </table>
+              </div>
+              <div class="container" style="background-color:#F8F8F8; border-width: 0px; font-weight: 500; margin-bottom: 20px;">
+                <table width="300">
+                  <tr>
+                    <td style="width: 170px;"><b>Jabatan</b></td>
+                    <td style="padding-left: 10px;"><?php ?></td>
+                  </tr>
+                </table>
+              </div>
+              <div style="margin-left: 470px; padding-top: 150px;">
+              <a href="<?php base_url() ?>home">
+                <button type="button" class="btn btn-danger"><img src="<?php echo base_url()?>img/icon/keluar.png" style="max-width: 30px; max-height: 60px;">Keluar</button>
+              </a>
+            </div>
           </div>
         </div>
+      </div>
+    </div>
+  </div>
+</div>
