256472ad280c2ff21623d078b23debe8be2cd0a9
nadiasitinamira <nadiasitinamira@gmail.com>
2019-06-04 17:41:56 +0700

    NTAAPPSSSS

--- application/views/userview/daftarkoordinator.php
+++ application/views/userview/daftarkoordinator.php
@@ -1,122 +1,147 @@
-<style> 
-
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-  background-color: white;
-  padding-right: 110px;
-  font-family: calibri;
-  font-size: 24px;
-
-}
-
-form {
-    overflow: hidden;
-}
- 
-label {
-    float: left;
-    width: 120px;
-    padding-right: 24px;
-    font-family: calibri; 
-
-}
- 
-input {
-    float: left;
-    width: calc(100% - 200px);
-    margin-bottom: 10px;
-    border:solid 1px;
-}
- 
-button {
-    float: right;
-    width: calc(100% - 200px);
-}
-
-</style>
 <div class="row">
   <div class="col-md-12">
-    <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2.5em; border:solid 1px;margin-bottom: 10px;">
-      <div class="row" style="margin-left: 10px; margin-right: 100px;">
-        <table id="myTable">
-            <form>
-    <label for="hari_tanggal" class="hari-tanggal">Hari/Tanggal</label>
-    <input id="hari_anggal" type="text">
- 
-    <label for="jam" class="jam">Jam</label>
-    <input id="jam" type="text">
-
-    <label for="tgl" class="tanggal">Tanggal</label>
-    <input id="tgl" type="text">
- 
-</form>
-      </table>
+    <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2.5em">
+      <div class="row" style="margin-left: 5px;">
+        <span style="background-color: #2F669F; height: 25px; width: 15px; margin-top: 7px; margin-left: 5px;"></span>
+        <span style="background-color: #2F669F; height: 25px; width: 5px; margin-top: 7px; margin-left: 5px;"></span> &nbsp;&nbsp;
+        <h3 style="color: #2F669F;"><b>Daftar Koordinator</b></h3>  
       </div>  
     </div>
   </div>
 </div>
-<a href="<?php base_url() ?>tambah">
-<div class="container" style="background-color:#b4b4b4; border-width: 0px; border-radius: 10px; height:40px;  width: 150px; margin-left: 0px;margin-bottom: 10px;">
-<img src="<?php echo base_url()?>img/icon/tambah.png" style="margin-top:5px;margin-bottom:10px;margin-left:5px;max-width: 30px; max-height: 100px;">&nbsp;&nbsp;&nbsp;Tambah
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 20px; margin-top: 25px; padding-top: 25px;">
+      <center><table>
+        <tr>
+          <td style="font-family: calibri; font-size: 20px;">Hari/Tanggal</td>
+          <td><input class="form-control" type="date" name="date" style="padding-right: 200px; margin-left: 30px; margin-bottom: 5px;"></td>
+        </tr>
+        <tr>
+          <td style="font-family: calibri; font-size: 20px;">Jam</td>
+          <td><input class="form-control" type="time" name="jam" style="padding-right: 200px; margin-left: 30px; margin-bottom: 5px;"></td>
+          <td style="padding-left: 100px;"><button class="btn btn-primary my-2 my-sm-0" type="submit"><img src="<?php echo base_url()?>img\icon\search.png" style="width: 20px; height: 20px;">&nbsp;&nbsp;Cari</button></td>
+        </tr>
+        <tr>
+          <td style="font-family: calibri; font-size: 20px;">Tempat</td>
+          <td><input class="form-control" placeholder="Ketik Tempat" type="text" name="tempat" style="padding-right: 200px; margin-left: 30px; margin-bottom: 5px;"></td>
+        </tr>
+      </table></center>
+    </div>
+  </div>
 </div>
-</a>
-<table>
-  <tr>
-    <th>No</th>
-    <th>NPU/NIP</th>
-    <th>Nama Pengawas</th>
-    <th>Jabatan</th>
-    <th>Tempat</th>
-    <th>Aksi</th>
-  </tr>
-  <tr>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></td>
-  </tr>
-  <tr>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height:30px;"></td>
-  </tr>
-  <tr>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></td>
-  </tr>
-  <tr>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></td>
-  </tr>
-  <tr>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></td>
-  </tr>
-  <tr>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td>test</td>
-    <td><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></td>
-  </tr>
-</table>
-        
+
+<div class="row">
+<div class="container" style="margin-top: 10px;">
+  <a href="<?php base_url() ?>edittambahpengawas">
+    <button class="btn btn-secondary my-2 my-sm-0" type="submit"><img src="<?php echo base_url()?>img\icon\tambah.png" style="width: 20px; height: 20px; padding-top: -1px;">&nbsp;&nbsp;Tambah</button>
+  </a>
+</div>
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 10px; margin-top: 10px; padding-top: 25px;">
+      <form><center>
+        <table border="2" style="width:1000px; text-align: center; margin-bottom: 20px;">
+          <tr>
+            <th style="font-size: 25px; font-family: calibri;">No</th>
+            <th style="font-size: 25px; font-family: calibri;">NPU/NIP</th>
+            <th style="font-size: 25px; font-family: calibri;">Nama Pengawas</th>
+            <th style="font-size: 25px; font-family: calibri;">Jabatan</th>
+            <th style="font-size: 25px; font-family: calibri;">Tempat</th>
+            <th style="font-size: 25px; font-family: calibri;">Aksi</th>
+          </tr>
+          <tr>
+            <td>1.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height:30px;"></a></td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>4.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>5.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>6.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>7.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>8.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>9.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>10.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+          <tr>
+            <td>11.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a href="<?php base_url() ?>edittambahpengawas"><img src="<?php echo base_url()?>img/icon/edit.png" style="margin-top:0px;margin-left:0px;max-width:30px; max-height: 30px;"></a></td>
+          </tr>
+        </table>
+      </center></form>
+    </div>
+  </div>
+</div>
