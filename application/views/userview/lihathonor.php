60e09c20b0b4c5b2a7dc0ffd7950b84eda10c678
nadiasitinamira <nadiasitinamira@gmail.com>
2019-06-07 12:20:27 +0700

    gini kan yakk

--- application/views/userview/lihathonor.php
+++ application/views/userview/lihathonor.php
@@ -4,7 +4,7 @@
       <div class="row" style="margin-left: 5px;">
         <span style="background-color: #2F669F; height: 25px; width: 5px; margin-top: 3px; margin-left: 5px;"></span>
         <span style="background-color: #2F669F; height: 25px; width: 2px; margin-top: 3px; margin-left: 2px;"></span> &nbsp;
-        <h4 style="color: #2F669F;"><b>Honor</b></h4>  
+        <h4 style="color: #2F669F;"><b>Honor</b></h4>
       </div>  
     </div>
   </div>
@@ -13,14 +13,23 @@
   <div class="col-md-12">
     <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 50px;">
       <div class="row" style="margin-left: 60px;" >
-
-        <!-- isi disini tampilan honor -->
-
-
-        <div class="row">
+        <div class="container" style=" border-width: 0px; margin-bottom: 20px; margin-top: 40px;">
+        <center><table>
+          <tr>
+            <td><b>NIP</b></td>
+              <td style="padding-left: 10px;"><input type="text" name="nip" placeholder="Masukkan NIP Anda" style="background-color:#C8C8C8;width: 250px; height: 30px;"><?php ?></td>
+              <td><a href="<?php base_url() ?>"><img src="img\icon\search.png" style="width: 20px; height: 20px;"></a></td>
+          </tr>
+        </table></center>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
   <div class="col-md-12">
-    <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 10px; margin-top: 10px; padding-top: 25px;">
-      <form><center>
+    <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 50px;">
+      <center><form>
         <table border="2" style="width:930px; text-align: center; margin-bottom: 20px;">
           <tr>
             <th style="font-size: 25px; font-family: calibri;">No</th>
@@ -107,12 +116,8 @@
             <td>test</td>
           </tr>
         </table>
-      </center></form>
-    </div>
-  </div>
-</div>
-<td style="padding-left: 230px;"><a href="<?php base_url() ?>halamanreport"><button type="button" class="btn btn-danger" style="margin-left: 825px;"><img src="<?php echo base_url()?>img\icon\keluar.png" style="width: 40px; height: 30px;">&nbsp;&nbsp;Keluar</button></a></td>
-      </div>
+      </form></center>
+      <td style="padding-left: 230px;"><a href="<?php base_url() ?>halamanreport"><button type="button" class="btn btn-danger" style="margin-left: 885px;"><img src="<?php echo base_url()?>img\icon\keluar.png" style="width: 40px; height: 30px;">&nbsp;&nbsp;Keluar</button></a></td>
     </div>
   </div>
 </div>
