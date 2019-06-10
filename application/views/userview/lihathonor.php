e628215a63e7accfaacea92048972e7f65837af7
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-06-10 23:15:57 +0700

    a

--- application/views/userview/lihathonor.php
+++ application/views/userview/lihathonor.php
@@ -1,10 +1,18 @@
 <div class="row">
   <div class="col-md-12">
+    <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2.5em">
+      <div class="row" style="margin-left: 5px;">
+        <span style="background-color: #2F669F; height: 25px; width: 15px; margin-top: 7px; margin-left: 5px;"></span>
+        <span style="background-color: #2F669F; height: 25px; width: 5px; margin-top: 7px; margin-left: 5px;"></span> &nbsp;&nbsp;
+        <h3 style="color: #2F669F;"><b>Honor</b></h3>  
+      </div>  
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
     <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 10px;padding-bottom: 7px;margin-top: 2.5em">
       <div class="row" style="margin-left: 5px;">
-        <span style="background-color: #2F669F; height: 25px; width: 5px; margin-top: 3px; margin-left: 5px;"></span>
-        <span style="background-color: #2F669F; height: 25px; width: 2px; margin-top: 3px; margin-left: 2px;"></span> &nbsp;
-        <h4 style="color: #2F669F;"><b>Honor</b></h4>
       </div>  
     </div>
   </div>
