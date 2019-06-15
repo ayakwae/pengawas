b949f499f12485595b2456a97d15c0064df99d1c
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-06-16 01:49:28 +0700

    hai

---
+++ application/views/userview/daftarinstansi.php
@@ -0,0 +1,138 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2.5em">
+      <div class="row" style="margin-left: 5px;">
+        <span style="background-color: #2F669F; height: 25px; width: 15px; margin-top: 7px; margin-left: 5px;"></span>
+        <span style="background-color: #2F669F; height: 25px; width: 5px; margin-top: 7px; margin-left: 5px;"></span> &nbsp;&nbsp;
+        <h3 style="color: #2F669F;"><b>Daftar Instansi</b></h3>  
+      </div>  
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 10px;padding-bottom: 7px;margin-top: 2.5em">
+      <div class="row" style="margin-left: 5px;">
+      </div>  
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 50px;">
+      <center><form>
+        <table border="2" style="width:930px; text-align: center; margin-bottom: 20px;">
+          <tr>
+            <th style="font-size: 25px; font-family: calibri;">No</th>
+            <th style="font-size: 25px; font-family: calibri;">ID</th>
+            <th style="font-size: 25px; font-family: calibri;">Kode Instansi</th>
+            <th style="font-size: 25px; font-family: calibri;">Nama Instansi</th>
+            <th style="font-size: 25px; font-family: calibri;">Singkatan</th>
+            <th style="font-size: 25px; font-family: calibri;">Alamat Instansi</th>
+            <th style="font-size: 25px; font-family: calibri;">Kode Pos</th>
+          </tr>
+          <tr>
+            <td>1.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>4.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>5.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>6.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>7.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>8.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>9.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>10.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>11.</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+        </table>
+      </form></center>
+      <td style="padding-left: 230px;"><a href="<?php base_url() ?>daftartempatujian"><img src="<?php echo base_url()?>img\icon\keluar1.png" style="width: 50px; height: 40px;margin-left: 960px;"></button></a></td>
+    </div>
+  </div>
+</div>
