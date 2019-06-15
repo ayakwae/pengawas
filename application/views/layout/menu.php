c753972fbda244d4f25c1ade8dadc434b0cb5c66
nadiasitinamira <nadiasitinamira@gmail.com>
2019-06-15 15:57:37 +0700

    Update menu.php

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -1,3 +1,30 @@
+<style type="text/css">
+.dropdown {
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding-left: 1px; padding-right: 1px;">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -37,9 +64,14 @@
       <li class="nav-item">
         <p class="navbar-nav" style="padding-top: 5px; color: gray; margin-right: 0px;">|</p>
       </li>
-      <li class="nav-item text-expanded">
-        <a class="nav-link" href="<?php echo base_url()?>halamanprofil" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-left: 25px; margin-right: 25px;">Profil</a>
+      <div class="dropdown">
+      <li class="nav-item" >
+        <u class="nav-link" href="<?php echo base_url()?>halamanprofil" style="font-family: calibri; font-size: 20px; margin-top: -5px; margin-left: 25px; margin-right: 25px;"><?= $this->fungsi->user_login()->username ?></u>
+          <div class="dropdown-content">
+            <a href="<?= site_url('auth/logout') ?>">Keluar</a>
+          </div>
       </li>
+      </div>
       <li class="nav-item">
         <p class="navbar-nav" style="padding-top: 5px; color: gray;">|</p>
       </li>
