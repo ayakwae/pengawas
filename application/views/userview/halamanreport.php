0b40289bb0172b24bbc1c598ff31fda1e7da6cac
nadiasitinamira <nadiasitinamira@gmail.com>
2019-06-27 21:31:02 +0700

    report

--- application/views/userview/halamanreport.php
+++ application/views/userview/halamanreport.php
@@ -13,8 +13,38 @@
   <div class="col-md-12">
     <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 50px;">
       <div class="row" style="margin-top: 25px; margin-left: 60px;" >
+        <a href="<?php base_url() ?>lihatalbum">
+          <div style="margin-left: 100px; margin-top: 70px; margin-bottom: 15px;">
+            <div style="background-color: #D0D0D0; width: 200px; height: 200px;">
+              <center><img src="<?php echo base_url()?>img\icon\album.png" style="width: 200px; height: 200px;"></center>
+            </div>
+            <div style="background:#E8E8E8; height: 60px;  padding-top: 15px; width: 200px;">
+              <h5 align="center"><b>Album</b></h5>
+            </div>
+          </div>
+        </a>
+        <a href="<?php base_url() ?>lihatbadge">
+          <div style="margin-left: 100px; margin-top: 70px; margin-bottom: 15px;">
+            <div style="background-color: #D0D0D0; width: 200px; height: 200px;">
+              <center><img src="<?php echo base_url()?>img\icon\badge.png" style="width: 200px; height: 200px;"></center>
+            </div>
+            <div style="background:#E8E8E8; height: 60px;  padding-top: 15px; width: 200px;">
+              <h5 align="center"><b>Badge</b></h5>
+            </div>
+          </div>
+        </a>
+        <a href="<?php base_url() ?>lihathonor">
+          <div style="margin-left: 100px; margin-top: 70px; margin-bottom: 15px;">
+            <div style="background-color: #D0D0D0; width: 200px; height: 200px;">
+              <center><img src="<?php echo base_url()?>img\icon\honor1.png" style="width: 200px; height: 200px;"></center>
+            </div>
+            <div style="background:#E8E8E8; height: 60px;  padding-top: 15px; width: 200px;">
+              <h5 align="center"><b>Honor</b></h5>
+            </div>
+          </div>
+        </a>
         <a href="<?php base_url() ?>lihatjadwal">
-          <div style="margin-left: 230px; margin-top: 70px; margin-bottom: 15px;">
+          <div style="margin-left: 230px; margin-top: 30px; margin-bottom: 15px;">
             <div style="background-color: #D0D0D0; width: 200px; height: 200px;">
               <center><img src="<?php echo base_url()?>img\icon\jadwal.png" style="width: 180px; height: 180px; margin-top: 10px;"></center>
             </div>
@@ -24,7 +54,7 @@
           </div>
         </a>
         <a href="<?php base_url() ?>lihatpresensi">
-          <div style="margin-left: 100px; margin-top: 70px; margin-bottom: 15px;">
+          <div style="margin-left: 100px; margin-top: 30px; margin-bottom: 15px;">
             <div style="background-color: #D0D0D0; width: 200px; height: 200px;">
               <center><img src="<?php echo base_url()?>img\icon\presensii.png" style="width: 200px; height: 200px;"></center>
             </div>
@@ -33,7 +63,6 @@
             </div>
           </div>
         </a>
-        <td style="padding-left: 230px;"><a href="<?php base_url() ?>lihathonor"><img src="<?php echo base_url()?>img\icon\keluar1.png" style="width: 50px; height: 40px;margin-left: 960px;"></button></a></td>
       </div>
     </div>
   </div>
