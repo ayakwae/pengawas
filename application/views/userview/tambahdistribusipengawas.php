25cf04b13e19f0f3111a87551a36d21e56703b75
nadiasitinamira <nadiasitinamira@gmail.com>
2019-07-01 20:12:57 +0700

    Update tambahdistribusipengawas.php

--- application/views/userview/tambahdistribusipengawas.php
+++ application/views/userview/tambahdistribusipengawas.php
@@ -15,7 +15,7 @@
       <table>
         <tr>
           <td style="font-family: calibri; font-size: 20px;">Kode Distribusi Pengawas Pelatihan</td>
-          <td><input class="form-control" placeholder="Ketik NPU" type="text" name="npu" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;margin-bottom: 15px;"></td>
+          <td><input class="form-control" placeholder="Ketik Kode Distribusi Pengawas Pelatihan" type="text" name="kode_distribusi_peng_pelatihan" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;margin-bottom: 15px;"></td>
         </tr>
         <tr>
           <td style="font-family: calibri; font-size: 20px;">Kode Lokasi</td>
@@ -32,32 +32,41 @@
         </tr>
         <tr>
           <td style="font-family: calibri; font-size: 20px;">Nomor Ruangan</td>
-          <td><input class="form-control" placeholder="Ketik Nama Pengawas" type="text" name="nama_pengawas" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;"></td>
-          <td style="padding-left: 230px;"><button class="btn btn-primary my-2 my-sm-0" type="submit"><img src="<?php echo base_url()?>img\icon\simpan.png" style="width: 50px; height: 50px;">&nbsp;&nbsp;Simpan</button></td>
+          <td>
+            <select name="kode_lokasi" class="form-control" style="width: 140px; margin-left: 50px;margin-bottom: 15px;">
+            <option value="kode_lokasi">&nbsp;&nbsp;&nbsp;&nbsp;-Pilih-</option>
+            <option value="1">L001</option>
+            <option value="2">L002</option>
+            <option value="3">L003</option>
+            <option value="4">L004</option>
+            <option value="5">L005</option>
+            </select>
+          </td>
+          <td style="padding-left: 230px;"><button class="btn btn-primary my-2 my-sm-0" type="submit"><img src="<?php echo base_url()?>img\icon\simpan.png" style="width: 50px; height: 50px;">&nbsp;&nbsp;&nbsp;Simpan</button></td>
         </tr>
         <tr>
           <td style="font-family: calibri; font-size: 20px;">NPU</td>
-          <td><input class="form-control" placeholder="Ketik Gelar Depan" type="text" name="gelar_depan" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;"></td>
+          <td><input class="form-control" placeholder="Ketik NPU" type="text" name="npu" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;"></td>
         </tr>
         <tr>
           <td style="font-family: calibri; font-size: 20px;">Jabatan</td>
-          <td><input class="form-control" placeholder="Ketik Gelar Belakang" type="text" name="gelar_belakang" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;"></td>
-          <td style="padding-left: 230px;"><a href="<?php base_url() ?>distribusipengawas"><button type="button" class="btn btn-danger"><img src="<?php echo base_url()?>img\icon\keluar.png" style="width: 60px; height: 50px;">&nbsp;&nbsp;Keluar</button></a></td>
+          <td><input class="form-control" placeholder="Ketik Jabatan" type="text" name="jabatan" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;"></td>
+          <td style="padding-left: 230px;"><a href="<?php base_url() ?>distribusipengawas"><button type="button" class="btn btn-danger"><img src="<?php echo base_url()?>img\icon\keluar.png" style="width: 60px; height: 50px;">&nbsp;Kembali</button></a></td>
         </tr>
         <tr>
           <td style="font-family: calibri; font-size: 20px;">Tanggal Coaching</td>
-          <td><input class="form-control" placeholder="Ketik Kode Instansi" type="text" name="kode_instansi" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;"></td>
+          <td><input class="form-control" placeholder="Ketik Kode Instansi" type="date" name="date" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;"></td>
         </tr>
         <tr>
           <td style="font-family: calibri; font-size: 20px;">Waktu Coaching</td>
-          <td><input class="form-control" placeholder="Ketik NPWP" type="text" name="npwp" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;margin-bottom: 15px;"></td>
+          <td><input class="form-control" placeholder="Ketik Waktu Coaching" type="text" name="waktu" style="padding-right: 200px; margin-left: 50px; margin-bottom: 5px;margin-bottom: 15px;"></td>
         </tr>
       </table>
     </div>
   </div>
 </div>
 
-<div class="row">
+<!-- <div class="row">
   <div class="col-md-12">
     <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 10px; margin-top: 10px; padding-top: 25px;">
       <form><center>
@@ -176,4 +185,4 @@
       </center></form>
     </div>
   </div>
-</div>
+</div> -->
