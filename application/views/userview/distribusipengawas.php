b57436b590c8ffd5e313e80a2695f9a9797ac0dd
nadiasitinamira <nadiasitinamira@gmail.com>
2019-07-01 20:32:21 +0700

    Update distribusipengawas.php

--- application/views/userview/distribusipengawas.php
+++ application/views/userview/distribusipengawas.php
@@ -21,16 +21,16 @@
   <div class="col-md-12">
     <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 10px; margin-top: 10px; padding-top: 25px;">
       <center><form>
-        <table border="2" style="width:930px; text-align: center; margin-bottom: 20px;">
+        <table border="2" style="width:1050px; text-align: center; margin-bottom: 20px;">
           <tr>
             <th style="font-size: 20px; font-family: calibri;">No</th>
             <th style="font-size: 20px; font-family: calibri;">Kode Distribusi Pengawas Pelatihan</th>
-            <th style="font-size: 20px; font-family: calibri;">Kode Lokasi</th>
-            <th style="font-size: 20px; font-family: calibri;">Nomor Ruang</th>
-            <th style="font-size: 20px; font-family: calibri;">NPU</th>
-            <th style="font-size: 20px; font-family: calibri;">Jabatan</th>
-            <th style="font-size: 20px; font-family: calibri;">Tanggal Coaching</th>
-            <th style="font-size: 20px; font-family: calibri;">Waktu Coaching</th>
+            <th style="font-size: 20px; font-family: calibri; padding-left: 20px; padding-right: 20px;">Kode Lokasi</th>
+            <th style="font-size: 20px; font-family: calibri; padding-left: 20px; padding-right: 20px;">Nomor Ruang</th>
+            <th style="font-size: 20px; font-family: calibri; padding-left: 40px; padding-right: 40px;">NPU</th>
+            <th style="font-size: 20px; font-family: calibri; padding-left: 50px; padding-right: 50px;">Jabatan</th>
+            <th style="font-size: 20px; font-family: calibri; padding-left: 30px; padding-right: 30px;">Tanggal Coaching</th>
+            <th style="font-size: 20px; font-family: calibri; padding-left: 20px; padding-right: 20px;">Waktu Coaching</th>
           </tr>
           <tr>
             <td>1.</td>
@@ -216,13 +216,13 @@
       </form></center>
       <table>
         <tr>
-          <td>
+          <td style="padding-left: 10px;">
             <a href="<?php base_url() ?>tambahdistribusipengawas">
               <button class="btn btn-secondary my-2 my-sm-0" type="submit"><img src="<?php echo base_url()?>img\icon\tambah.png" style="width: 20px; height: 20px; padding-top: -1px;">&nbsp;&nbsp;Tambah</button>
             </a>
           </td>
           <td>
-            <a href="<?php base_url() ?>pengawas"><img src="<?php echo base_url()?>img\icon\keluar1.png" style="width: 50px; height: 40px;margin-left: 910px;"></button>
+            <a href="<?php base_url() ?>pengawas"><img src="<?php echo base_url()?>img\icon\keluar1.png" style="width: 50px; height: 40px;margin-left: 900px;"></button>
             </a>
           </td>
         </tr>
