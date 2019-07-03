ee4f46ecd623e9f70f74f0f09290b7d36ffa44d4
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-07-03 22:17:45 +0700

    ghi

--- application/views/userview/saintekdaftarkoordinator.php
+++ application/views/userview/saintekdaftarkoordinator.php
@@ -16,8 +16,8 @@
       <div class="row" style="margin-top: -10px; margin-left: 60px">
         <a href="<?php base_url() ?>saintektabeldaftarkoordinator">
           <div style="margin-left: 90px; margin-top: 30px; margin-bottom: 15px;">
-            <div style="background-color: #D0D0D0; width: 200px; height: 200px;">
-              <center><img src="<?php echo base_url()?>img\icon\" style="width: 230px; height: 200px;"></center>
+            <div style="background-color: #D0D0D0; width: 230px; height: 200px;">
+              <center><img src="<?php echo base_url()?>img\icon\koordinator.png" style="width: 200px; height: 200px;"></center>
             </div>
             <div style="background:#E8E8E8; height: 60px;  padding-top: 15px; width: 230px;">
               <h5 align="center"><b>Koordinator</b></h5>
@@ -27,7 +27,7 @@
         <a href="<?php base_url() ?>saintektabeldaftarkoordinatorutama">
           <div style="margin-left: 70px; margin-top: 30px; margin-bottom: 15px;">
             <div style="background-color: #D0D0D0; width: 200px; height: 200px;">
-              <center><img src="<?php echo base_url()?>img\icon\" style="width: 200px; height: 200px;"></center>
+              <center><img src="<?php echo base_url()?>img\icon\koordinatorutama.png" style="width: 200px; height: 200px;"></center>
             </div>
             <div style="background:#E8E8E8; height: 60px;  padding-top: 15px; width: 200px;">
               <h5 align="center"><b>Koordinator Utama</b></h5>
