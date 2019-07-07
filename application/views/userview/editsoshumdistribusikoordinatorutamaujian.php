821a2ca48155299b163b913e7a7130dea0f27278
nadiasitinamira <nadiasitinamira@gmail.com>
2019-07-07 11:42:39 +0700

    addsa

---
+++ application/views/userview/editsoshumdistribusikoordinatorutamaujian.php
@@ -0,0 +1,41 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;margin-top: 2.5em">
+      <div class="row" style="margin-left: 5px;">
+        <span style="background-color: #2F669F; height: 20px; width: 5px; margin-top: 8px; margin-left: 5px;"></span>
+        <span style="background-color: #2F669F; height: 20px; width: 3px; margin-top: 8px; margin-left: 3px;"></span> &nbsp;
+        <b style="color: #2F669F; font-size: 22px;">Edit Distribusi Koordinator Utama Ujian (SOSHUM)</b>
+      </div>  
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 20px; margin-top: 25px; padding-top: 25px;">
+      <table>
+        <tr>
+          <td style="font-family: calibri; font-size: 20px;">Nama Lokasi</td>
+          <td><input class="form-control" placeholder="Ketik Nama Lokasi" type="text" name="npu" style="padding-right: 200px; margin-left: 30px; margin-bottom: 5px;"></td>
+        </tr>
+        <tr>
+          <td style="font-family: calibri; font-size: 20px;">Nama Koordinator</td>
+          <td><input class="form-control" placeholder="Ketik Nama Koordinator" type="text" name="nama_koordinator" style="padding-right: 200px; margin-left: 30px; margin-bottom: 5px;"></td>
+           <td style="padding-left: 230px;"><button class="btn btn-primary my-2 my-sm-0" type="submit"><img src="<?php echo base_url()?>img\icon\simpan.png" style="width: 50px; height: 50px;">&nbsp;&nbsp;&nbsp;Simpan</button></td>
+        </tr>
+        <tr>
+          <td style="font-family: calibri; font-size: 20px;">Jabatan</td>
+          <td><input class="form-control" placeholder="Ketik Jabatan" type="text" name="jabatan" style="padding-right: 200px; margin-left: 30px; margin-bottom: 5px;"></td>
+          <td style="padding-left: 230px;"><a href="<?php base_url() ?>soshumtabelkoordinatorutamaujian"><button type="button" class="btn btn-danger"><img src="<?php echo base_url()?>img\icon\keluar.png" style="width: 60px; height: 50px;">Kembali</button></a></td>
+        </tr>
+        <tr>
+          <td style="font-family: calibri; font-size: 20px;">Tanggal Coaching</td>
+          <td><input class="form-control" placeholder="Ketik Tanggal Coaching" type="date" name="tanggal_coaching" style="padding-right: 200px; margin-left: 30px; margin-bottom: 5px;"></td>
+        </tr>
+        <tr>
+          <td style="font-family: calibri; font-size: 20px;">Waktu Coaching</td>
+          <td><input class="form-control" placeholder="Ketik Waktu Coaching" type="text" name="waktu_coaching" style="padding-right: 200px; margin-left: 30px; margin-bottom: 5px;margin-bottom: 15px;"></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
